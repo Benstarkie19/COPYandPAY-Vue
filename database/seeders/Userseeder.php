ecd7ac87e0df54d7546b34263c9e046015558933
Ben <benstarkie19@gmail.com>
2021-03-18 12:35:51 +0000

    added user  seeder  for  testing

---
+++ database/seeders/Userseeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'ben',
+            'email' => 'ben@gmail.com',
+            'password' => Hash::make('ben')
+        ]);
+    }
+}
