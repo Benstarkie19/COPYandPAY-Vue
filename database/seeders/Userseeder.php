fac2fc19ba476de57c6a1865e2a8b57769cffc3b
Ben <benstarkie19@gmail.com>
2021-03-18 12:36:22 +0000

    password

--- database/seeders/Userseeder.php
+++ database/seeders/Userseeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'ben',
             'email' => 'ben@gmail.com',
-            'password' => Hash::make('ben')
+            'password' => Hash::make('12345678')
         ]);
     }
 }
